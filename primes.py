b9b84da6c9bda1890bb67f122444164a359aa48f
Arham Zahid <arham.zahid@kcl.ac.uk>
2023-10-06 08:29:21 +0100

    Updated solution for the primes exercise

--- primes.py
+++ primes.py
@@ -3,4 +3,28 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError(f'{number_of_primes} is less than or equal to zero so can\'t be prime')
+    num = 2
+
+    while len(list) < number_of_primes:
+        if num <= 3:
+            is_prime = True
+        else:
+            if num % 2 == 0 or num % 3 == 0:
+                is_prime = False
+            else:
+                is_prime = True
+                i = 5
+                while i * i <= num:
+                    if num % i == 0 or num % (i + 2) == 0:
+                        is_prime = False
+                        break
+                    i = i + 6
+
+        if is_prime:  # No need to check == True, it's redundant
+            list.append(num)
+
+        num = num + 1
+
     return list
